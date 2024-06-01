1e1df53db9e2ad3f936b051b2666edaccaaeac59
kybaq <flfsk12@gmail.com>
2024-06-01 13:13:30 +0900

    init: dev branch

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainPage() {
+  return (
+    <div>
+      게시글 검색
+      <input type="text" placeholder="검색어를 입력하세요" />
+    </div>
+  );
+}
+
+export default MainPage;
