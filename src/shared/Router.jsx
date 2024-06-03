fcd660663edb5f45657017d0a1ee33cece7ad2c1
kybaq <flfsk12@gmail.com>
2024-06-03 23:28:13 +0900

    feat: outlet 추가 통한 중첩 라우팅

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 import PostPage from '../pages/PostPage';
+import PostViewModal from '../components/PostViewModal';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="posting/" element={<PostPage />} />
+        <Route path="/" element={<MainPage />}>
+          <Route path="detail/:id" element={<PostViewModal />} />
+        </Route>
+        <Route path="posting" element={<PostPage />} />
       </Routes>
     </BrowserRouter>
   );
