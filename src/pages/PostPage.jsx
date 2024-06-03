db99db93fe5a619830bdc8f2f51673df1bd6251b
kybaq <flfsk12@gmail.com>
2024-06-04 00:46:09 +0900

    style: cursor 속성 추가

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { supabase } from '../shared/supabaseClient';
 
 function PostPage() {
+  const getPosts = async () => {
+    const { data, error } = await supabase.from('posts').select();
+    console.log(data);
+  };
+
+  getPosts();
+
   return <div>PostPage</div>;
 }
 
