ae8e79638642ce1024db49be5c1f7aa61d6961a2
kybaq <flfsk12@gmail.com>
2024-06-03 17:48:39 +0900

    update: Global Style, Router, redux store provider 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Router from './shared/Router';
 import GlobalStyle from './GlobalStyle';
+import { Provider } from 'react-redux';
+import store from './redux/config/configStore';
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <Router />
-    </>
+    </Provider>
   );
 };
 
