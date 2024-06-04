8efcb733029cc4d57d910ff34d5a2cb68f8ae872
kybaq <flfsk12@gmail.com>
2024-06-04 20:55:35 +0900

    fix: 불필요한 import 삭제

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,13 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
-import Detail from '../pages/Detail';
 import Post from '../pages/Post';
 import PostViewModal from '../components/PostViewModal';
-import GlobalStyle from '../GlobalStyle';
-import Mypage from '../pages/Mypage';
-import Myposts from '../pages/Myposts';
+import MyPage from '../pages/MyPage';
+import MyPosts from '../pages/MyPosts';
 import Login from '../pages/Login';
-import Header from '../components/Header';
 
 function Router() {
   return (
@@ -19,8 +16,8 @@ function Router() {
         </Route>
         <Route path="post" element={<Post />} />
         {/* <Route path="/detail/:id" element={<Detail />} /> */}
-        <Route path="/mypage" element={<Mypage />} />
-        <Route path="/myposts" element={<Myposts />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/myposts" element={<MyPosts />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
