5e253b72e1a58c6776c3e7d10fb23e0b63a0eb03
kybaq <flfsk12@gmail.com>
2024-06-04 20:19:51 +0900

    fix: supabase export

--- src/shared/supabaseClient.js
+++ src/shared/supabaseClient.js
@@ -3,6 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_PROJECT_URL = import.meta.env.VITE_APP_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
 
-const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY);
-
-export default supabase;
+export const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY);
