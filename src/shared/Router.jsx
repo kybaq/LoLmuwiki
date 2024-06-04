1de3a96409afe1d0be884b3bf8b786f75fcb2f16
kybaq <flfsk12@gmail.com>
2024-06-04 11:48:03 +0900

    update: path 추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Detail from '../pages/Detail';
-import PostPage from '../pages/PostPage';
+import Post from '../pages/Post';
 import PostViewModal from '../components/PostViewModal';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Home />} >
+        <Route path="/" element={<Home />}>
           <Route path="detail/:id" element={<PostViewModal />} />
           {/* Detail 이란 이름으로 바꾸기 */}
         </Route>
-        <Route path="posting" element={<PostPage />} />
+        <Route path="post" element={<Post />} />
       </Routes>
     </BrowserRouter>
   );
