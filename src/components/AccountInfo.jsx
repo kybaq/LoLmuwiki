e20fe128ed609cb9fe16304217cd9e9b3d7143f1
Jaeyoung <103552850+Wodud5683@users.noreply.github.com>
2024-06-05 11:42:36 +0900

    feat: 닉네임 변경 로직 삭제

--- src/components/AccountInfo.jsx
+++ src/components/AccountInfo.jsx
@@ -1,58 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import AccountInfoItem from './AccountInfoItem';
 import ProfilePicture from './ProfilePicture';
-import { supabase } from '../shared/supabaseClient';
-import { login } from '../redux/slices/authSlice';
 
 const AccountInfoContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center; 
+  align-items: center;
   width: 100%;
   padding: 20px;
+  color: white;
 `;
 
 const AccountInfo = () => {
-  const dispatch = useDispatch();
-  const { id, email, full_name } = useSelector((state) => state.auth);
+  const { email, full_name } = useSelector((state) => state.auth);
 
-  const handleNicknameChange = async () => {
-    const newNickname = prompt('새로운 닉네임을 입력하세요:', full_name);
-    
-    if (newNickname === null) return; 
-    
-    if (newNickname !== full_name) {
-      if (newNickname.length > 8) {
-        alert('닉네임은 8자 이하로 설정해야 합니다.');
-        return;
-      }
-      try {
-        const { error } = await supabase
-          .from('users')
-          .update({ nickname: newNickname })
-          .eq('id', id);
-  
-        if (error) {
-          throw error;
-        }
-  
-        dispatch(login({ id, email, full_name: newNickname }));
-  
-        alert('닉네임이 성공적으로 변경되었습니다.');
-      } catch (error) {
-        console.error('닉네임 변경 중 오류:', error);
-        alert('닉네임 변경 중 오류가 발생했습니다.');
-      }
-    }
-  };
-  
   return (
     <AccountInfoContainer>
       <ProfilePicture />
       <AccountInfoItem label="이메일" value={email} editable={false} />
-      <AccountInfoItem label="닉네임" value={full_name} editable={true} onEdit={handleNicknameChange} />
+      <AccountInfoItem label="닉네임" value={full_name} editable={false} />
     </AccountInfoContainer>
   );
 };
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Post from '../pages/Post';
 import PostViewModal from '../components/PostViewModal';
-import MyPage from '../pages/MyPage';
+import Mypage from '../pages/Mypage';
 import MyPosts from '../pages/MyPosts';
 import Login from '../pages/Login';
 
@@ -16,7 +16,7 @@ function Router() {
         </Route>
         <Route path="post" element={<Post />} />
         {/* <Route path="/detail/:id" element={<Detail />} /> */}
-        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypage" element={<Mypage />} />
         <Route path="/myposts" element={<MyPosts />} />
         <Route path="/login" element={<Login />} />
       </Routes>
