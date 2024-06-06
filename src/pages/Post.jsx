ce56a4eb99bb4238f5e15e3315e64ac26793aec2
kybaq <flfsk12@gmail.com>
2024-06-07 01:30:47 +0900

    fix: 로그인 확인 기능 제거

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -110,7 +110,7 @@ const StSubmitBtn = styled.button`
 
 function Post() {
   const navigate = useNavigate();
-  const { id, full_name, isAuthenticated } = useSelector((state) => state.auth);
+  const { id, full_name } = useSelector((state) => state.auth);
 
   // 게시글 작성
   const titleRef = useRef(null);
