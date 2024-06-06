ac6266830ddf33f2565dc185a53dadaa9b38ea19
kybaq <flfsk12@gmail.com>
2024-06-06 19:41:59 +0900

    feat: 세션 정보를 users 테이블로 전송 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Header from '../components/Header';
 import LoginModal from '../components/LoginModal';
 import styled from 'styled-components';
 import Video from '../components/Video';
 import FeedList from '../components/FeedList';
+import { useEffect } from 'react';
 import { supabase } from '../shared/supabaseClient';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../redux/slices/authSlice';
 import ToTopButton from '../components/ToTopBtn';
 
@@ -18,7 +19,34 @@ const StMain = styled.main`
 const Home = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const dispatch = useDispatch();
-
+  const user = useSelector((state) => state.auth);
+  const saveUserToDatabase = async (user) => {
+    console.log('user::', user);
+    const userData = {
+      user_id: user.id,
+      full_name: user.full_name,
+      avatar_url: user.avatar_url,
+      email: user.email,
+      created_at: user.created_at,
+    };
+    console.log('user_id::', userData.user_id);
+    try {
+      const { error } = await supabase.from('users').upsert([userData]);
+      if (error) {
+        console.error(
+          '유저 정보를 데이터베이스에 저장하는 중 에러 발생:',
+          error.message,
+        );
+      } else {
+        console.log('유저 정보를 데이터베이스에 성공적으로 저장');
+      }
+    } catch (error) {
+      console.error(
+        '유저 정보를 데이터베이스에 저장하는 중 에러 발생:',
+        error.message,
+      );
+    }
+  };
   const openModal = () => {
     setIsModalOpen(true);
   };
@@ -27,21 +55,30 @@ const Home = () => {
   };
 
   const getUserSession = async () => {
-    const { data } = await supabase.auth.getSession();
+    const { data, error } = await supabase.auth.getSession();
+    if (error) {
+      console.error('Error fetching session:', error.message);
+      return null;
+    }
     return data;
   };
-  // const authData = useSelector((state) => state.auth);
-  // console.log('authData::', authData);
 
   useEffect(() => {
     const fetchData = async () => {
       const { session } = await getUserSession();
+      console.log('session::', session);
       if (session) {
         dispatch(login(session.user.identities[0]));
       }
     };
     fetchData();
-  }, []);
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (user.isAuthenticated) {
+      saveUserToDatabase(user);
+    }
+  }, [user]);
 
   return (
     <>
